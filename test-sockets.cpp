418a7ae70a552d860b4d271e11f057eeaf6e6a9e
remram44 <remirampin@gmail.com>
2009-10-04 12:51:02 +0200

    Updated test.
    --HG--
    extra : transplant_source : %93%B4T%3EfN%20%EF%DB%A6%B0%A1%81O%C7%AF%A5RnB

--- test-sockets.cpp
+++ test-sockets.cpp
@@ -7,27 +7,27 @@ int main()
     Socket::Init();
 
     try {
-        ServerSocket *server = ServerSocket::Listen(5001);
-        std::cerr << "Mode 1" << std::endl;
-        ClientSocket *inc = server->Accept(-1);
-        ClientSocket *out = ClientSocket::Connect("localhost", 5002);
-
-        inc->Send("Hello\n", 6);
-        int len = 0;
-        char rep[8];
-        while(len < 8)
-            len += out->Recv(rep+len, 8-len);
-        rep[7] = '\0';
-        std::cerr << rep << std::endl;
-
-        delete server;
-        delete inc;
-        delete out;
-    }
-    catch(std::exception &e)
-    {
         try {
-            std::cerr << "Mode 2" << std::endl;
+            ServerSocket *server = ServerSocket::Listen(5001);
+            std::cerr << "TCP: Mode 1" << std::endl;
+            ClientSocket *inc = server->Accept(-1);
+            ClientSocket *out = ClientSocket::Connect("localhost", 5002);
+
+            inc->Send("Hello\n", 6);
+            int len = 0;
+            char rep[8];
+            while(len < 8)
+                len += out->Recv(rep+len, 8-len);
+            rep[7] = '\0';
+            std::cerr << rep << std::endl;
+
+            delete server;
+            delete inc;
+            delete out;
+        }
+        catch(SocketCantUsePort &e)
+        {
+            std::cerr << "TCP: Mode 2" << std::endl;
             ServerSocket *server = ServerSocket::Listen(5002);
             ClientSocket *out = ClientSocket::Connect("localhost", 5001);
             ClientSocket *inc = server->Accept(-1);
@@ -44,12 +44,15 @@ int main()
             delete out;
             delete inc;
         }
-        catch(std::exception &e)
-        {
-            std::cerr << "Oops!" << std::endl;
-            return 1;
-        }
     }
+    catch(SocketError &e)
+    {
+        std::cerr << "TCP: Oops!" << std::endl;
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    std::cerr << "\nTCP testing finished\n\n";
 
     return 0;
 }
